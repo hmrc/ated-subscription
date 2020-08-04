33870bf6cf0101f6228df87332c61b53662708db
CameronStamp <68219671+CameronStamp@users.noreply.github.com>
2020-08-04 11:52:15 +0100

    DL-3907 play 2.7 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % "1.13.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "2.23.0"
   )
 
   def apply() = compile ++ Test()
@@ -20,12 +20,11 @@ object AppDependencies {
   object Test {
     def apply() = new TestDependencies {
       override lazy val test = Seq(
-        "org.scalatest" %% "scalatest" % "3.0.8" % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.3" % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-        "org.mockito" % "mockito-core" % "3.2.4" % scope,
-        "com.github.tomakehurst" % "wiremock-jre8" % "2.26.3" % IntegrationTest withSources()
+        "org.mockito" % "mockito-core" % "3.3.3" % scope,
+        "com.github.tomakehurst" % "wiremock-jre8" % "2.23.2" % IntegrationTest withSources()
       )
     }.test
   }
