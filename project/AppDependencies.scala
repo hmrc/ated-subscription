6266788395ff0a4689736555c0beef4603453ba6
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2022-08-16 10:43:04 +0100

    DL-8394 fix it tests and more dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,13 +21,13 @@ object AppDependencies {
     def apply(): Seq[sbt.ModuleID] = new TestDependencies {
       override lazy val test = Seq(
         "org.scalatestplus.play"       %% "scalatestplus-play"   % "5.1.0"             % scope,
-        "org.pegdown"                  % "pegdown"               % "1.6.0"             % scope,
+        "org.pegdown"                  %  "pegdown"              % "1.6.0"             % scope,
         "com.typesafe.play"            %% "play-test"            % PlayVersion.current % scope,
-        "org.mockito"                  % "mockito-core"          % "4.0.0"             % scope,
+        "org.mockito"                  %  "mockito-core"         % "4.7.0"             % scope,
         "org.scalatestplus"            %% "mockito-3-12"         % "3.2.10.0"          % scope,
-        "com.vladsch.flexmark"         %  "flexmark-all"         %  "0.35.10"          % scope,
-        "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.5"            % scope,
-        "com.github.tomakehurst"       % "wiremock-jre8"         % "2.31.0"            % IntegrationTest withSources()
+        "com.vladsch.flexmark"         %  "flexmark-all"         % "0.35.10"           % scope,
+        "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.3"            % scope,
+        "com.github.tomakehurst"       %  "wiremock-jre8"        % "2.33.2"            % IntegrationTest withSources()
       )
     }.test
   }
