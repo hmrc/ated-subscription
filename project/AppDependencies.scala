d30e5d528252bce9a4a5aa8b1ffc2a02e27cfc38
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-04-05 11:56:03 +0100

    DL-13467-removedunuseddependencies and updated akka in logback

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,26 +1,17 @@
+import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
-
-  import play.core.PlayVersion
-  import play.sbt.PlayImport.*
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % "8.5.0"
   )
-
   def apply(): Seq[ModuleID] = compile ++ test
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-test-play-30" % "8.5.0",
-    "org.scalatestplus.play"       %% "scalatestplus-play"     % "7.0.1",
-    "org.mockito"                  %  "mockito-core"           % "5.11.0" ,
-    "org.scalatestplus"            %% "mockito-4-11"           % "3.2.18.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.17.0"
   ).map(_ % Test)
 
-  val itDependencies: Seq[ModuleID] = Seq(
-    "org.wiremock" % "wiremock" % "3.4.2" % Test
-  )
+  val itDependencies: Seq[ModuleID] = Seq()
 }
--- project/TestPhases.scala
+++
@@ -1,6 +0,0 @@
-import sbt._
-
-object TestPhases {
-  lazy val TemplateTest = config("tt") extend Test
-  lazy val TemplateItTest = config("tit")
-}
